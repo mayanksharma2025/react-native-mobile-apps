e505575180f2c551ea6c9eba1606c057d3c58efa
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-08-06 17:07:54 +0530

    basic button

--- native-app/app/(tabs)/index.tsx
+++ native-app/app/(tabs)/index.tsx
@@ -1,41 +1,36 @@
-import React from 'react' // Import React library to use React components and hooks
-import {
-  View, // Import View component to create a container
-  Text, // Import Text component to display text
-} from 'react-native' // Import components from react-native library
+import React from 'react'
+import { StyleSheet, Button, Text, Alert, View } from 'react-native'
 
-// Define the main App component as the default export
-export default function App() {
-  // Return the UI of the App component
+function App() {
   return (
-    <View
-      style={{
-        flex: 1, // Set flex to 1 to fill the entire screen
-        justifyContent: 'center', // Center align items vertically
-        alignItems: 'center', // Center align items horizontally
-      }}
-    >
-      {/* Outer container view */}
-      <View
-        style={{
-          backgroundColor: '#FC6C85', // Set background color to green
-          height: 200, // Set height of the container to 200
-          width: 200, // Set width of the container to 200
-          justifyContent: 'center', // Center align items vertically
-          alignItems: 'center', // Center align items horizontally
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 20, // Set font size of the text to 20
-            color: 'white', // Set text color to white
-            textAlign: 'center', // Center align the text
-            margin: 10, // Add margin around the text
-          }}
-        >
-          Mayank Sharma {/* Text to be displayed */}
-        </Text>
-      </View>
+    <View style={styles.setMargin}>
+      <Text style={styles.header}>Simple Button</Text>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      <Text style={styles.header}>Coloured Button</Text>
+      <Button
+        title="Press me"
+        color="#f194ff"
+        onPress={() => Alert.alert('Coloured Button pressed')}
+      />
+      <Text style={styles.header}>Disabled Button</Text>
+      <Button
+        title="Press me"
+        disabled
+        onPress={() => Alert.alert('Cannot press this one')}
+      />
     </View>
   )
 }
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  setMargin: {
+    marginTop: 50,
+  },
+})
+export default App
