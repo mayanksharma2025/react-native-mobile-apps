5c655bfe9dbc79ed6ce2ba38079c0809a6001fb3
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-08-07 00:11:40 +0530

    random joke fetch api

--- native-app/app/(tabs)/index.tsx
+++ native-app/app/(tabs)/index.tsx
@@ -1,79 +1,86 @@
-// Import React library
-import React from 'react'
+import { useState } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
-import {
-  View,
-  TextInput,
-  ImageBackground,
-  StyleSheet,
-  Dimensions,
-} from 'react-native'
-
-const screenHeight = Dimensions.get('window').height
-
-const screenWidth = Dimensions.get('window').width
-
-const BackgroundImg = () => {
-  return (
-    <View>
-      <ImageBackground
-        source={{
-          uri: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1JZHht.img?w=768&h=432&m=6',
-        }}
-        resizeMode="stretch"
-        style={styles.img}
-      >
-        <TextInput placeholder="Mayank Sharma" style={styles.input} />
-        <TextInput placeholder="Mayank Sharma" style={styles.input} />
-      </ImageBackground>
-    </View>
-  )
-}
-
-// Define the main App component
 const App = () => {
+  const [joke, setJoke] = useState('')
+
+  const getJoke = async () => {
+    try {
+      const response = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+          Accept: 'application/json',
+        },
+      })
+
+      const data = await response?.json()
+
+      setJoke(data.joke)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
-    <View>
-      <BackgroundImg />
+    <View style={styles.container}>
+      <Text style={styles.title}>Random Jokes Generator</Text>
+
+      <View style={styles.jokeContainer}>
+        <Text>{joke}</Text>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={getJoke}>
+        <Text style={styles.buttonText}>Get a Joke</Text>
+      </TouchableOpacity>
     </View>
   )
 }
-export default App
 
-// Define styles for the components
 const styles = StyleSheet.create({
-  img: {
-    // Set the height to the screen height
-    height: screenHeight,
-
-    // Set the width to the screen width
-    width: screenWidth,
-
-    // Center content vertically
-    justifyContent: 'center',
-
-    // Center content horizontally
+  container: {
+    flex: 1,
+    backgroundColor: '#eee',
     alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
   },
-  input: {
-    // Set the height of the input field
-    height: 40,
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#333739',
+    marginBottom: 24,
+  },
+  jokeContainer: {
+    backgroundColor: 'white',
+    borderRadius: 15,
+    padding: 20,
+    marginBottom: 24,
+    width: '100%',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    shadowRadius: 15,
+    shadowOpacity: 1,
+    elevation: 4,
+  },
 
-    // Add margin around the input field
-    margin: 12,
-
-    // Set the border width
-    borderWidth: 2,
-
-    // Add padding inside the input field
-    padding: 10,
-
+  button: {
+    padding: 16,
     backgroundColor: '#FC6C85',
-
-    color: '#FFFFFF',
-
-    borderColor: '#FFFFFF',
-
-    textAlign: 'center',
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    shadowOpacity: 1,
+    elevation: 4,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
   },
 })
+
+export default App
