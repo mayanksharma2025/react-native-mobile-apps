e8c2571578bb170aa48e2d7d2da7726900b3d914
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-10-22 19:43:33 +0530

    Job board app

--- native-app/app/(tabs)/index.tsx
+++ native-app/app/(tabs)/index.tsx
@@ -1,170 +1,261 @@
 // App.js
+import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  FlatList,
+  TextInput,
+  Button,
+  ScrollView,
+  Modal,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native'
 
-// Import necessary modules and components
-import { useState } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import initialJobData from '../initialJobData'
+const JobBoard = () => {
+  const [jobData, setJobData] = useState(initialJobData)
+  const [showFilters, setShowFilters] = useState(true)
+  const [titleFilter, setTitleFilter] = useState('')
+  const [companyFilter, setCompanyFilter] = useState('')
+  const [locationFilter, setLocationFilter] = useState('')
+  const [selectedJob, setSelectedJob] = useState(null)
+  const [isModalVisible, setIsModalVisible] = useState(false)
 
-// Import TaskList component
-import TaskList from '../components/TaskList'
-
-// Import TaskModal component
-import TaskModal from '../components/TaskModal'
-import styles from '../components/styles' // Import styles
-
-// Define the main App component
-const App = () => {
-  // Define state variables
-
-  // Array to store tasks
-  const [tasks, setTasks] = useState([])
-  const [task, setTask] = useState({
-    title: '',
-    description: '',
-    status: 'Pending',
-    deadline: '',
-    createdAt: '',
-  })
-
-  // Task object for creating/editing tasks
-
-  // Modal visibility
-  const [modalVisible, setModalVisible] = useState(false)
-
-  // Task being edited
-  const [editingTask, setEditingTask] = useState(null)
-  const [validationError, setValidationError] = useState(false) // Validation flag
-
-  // Function to add a new task or update an existing task
-  const handleAddTask = () => {
-    if (task.title.trim() !== '' && task.deadline !== '') {
-      const currentDate = new Date()
-      const formattedDate = currentDate.toLocaleString()
-
-      if (editingTask) {
-        // If editing an existing task, update it
-        const updatedTasks = tasks.map((t) =>
-          t.id === editingTask.id ? { ...t, ...task } : t
-        )
-        setTasks(updatedTasks)
-        setEditingTask(null)
-      } else {
-        // If adding a new task, create it
-        const newTask = {
-          id: Date.now(),
-          ...task,
-
-          // Set the creation date and time as a string
-          createdAt: formattedDate,
-        }
-        setTasks([...tasks, newTask])
-      }
-
-      // Clear the task input fields and reset state
-      setTask({
-        title: '',
-        description: '',
-        status: 'Pending',
-        deadline: '',
-        createdAt: '',
-      })
-
-      // Close the modal
-      setModalVisible(false)
-
-      // Reset validation error
-      setValidationError(false)
-    } else {
-      // Show validation error if fields are not filled
-      setValidationError(true)
-    }
-  }
-
-  // Function to handle task editing
-  const handleEditTask = (task) => {
-    // Set the task being edited
-    setEditingTask(task)
-
-    // Pre-fill the input with task data
-    setTask(task)
-
-    // Open the modal for editing
-    setModalVisible(true)
-  }
-
-  // Function to delete a task
-  const handleDeleteTask = (taskId) => {
-    const updatedTasks = tasks.filter((t) => t.id !== taskId)
-    setTasks(updatedTasks)
-  }
-
-  // Function to toggle task completion status
-  const handleToggleCompletion = (taskId) => {
-    const updatedTasks = tasks.map((t) =>
-      t.id === taskId
-        ? {
-            ...t,
-            status: t.status === 'Pending' ? 'Completed' : 'Pending',
-          }
-        : t
+  const applyFilters = () => {
+    const filteredJobs = initialJobData.filter(
+      (job) =>
+        job.title.toLowerCase().includes(titleFilter.toLowerCase()) &&
+        job.company.toLowerCase().includes(companyFilter.toLowerCase()) &&
+        job.location.toLowerCase().includes(locationFilter.toLowerCase())
     )
-    setTasks(updatedTasks)
+    setJobData(filteredJobs)
+    setShowFilters(false)
   }
 
-  //  Return the JSX for rendering the component
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Task Manager</Text>
-      {/* Render the TaskList component */}
-      <TaskList
-        tasks={tasks}
-        handleEditTask={handleEditTask}
-        handleToggleCompletion={handleToggleCompletion}
-        handleDeleteTask={handleDeleteTask}
-      />
-      {/* Button to add or edit tasks */}
-      <TouchableOpacity
-        style={styles.addButton}
-        onPress={() => {
-          setEditingTask(null)
-          setTask({
-            title: '',
-            description: '',
-            status: 'Pending',
-            deadline: '',
-            createdAt: '',
-          })
-          setModalVisible(true)
-          setValidationError(false)
+  const toggleFilters = () => {
+    setShowFilters(!showFilters)
+  }
+
+  const renderJobItem = ({ item }) => (
+    <TouchableOpacity onPress={() => handleViewDetails(item)}>
+      <View style={styles.jobItem}>
+        <Text style={styles.jobTitle}>{item.title}</Text>
+        <Text style={styles.jobCompany}>{item.company}</Text>
+        <Text style={styles.jobLocation}>{item.location}</Text>
+        <Text style={styles.jobDescription}>{item.description}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+
+  const handleViewDetails = (job) => {
+    setSelectedJob(job)
+    setIsModalVisible(true)
+  }
+
+  const renderJobDetailsModal = () => {
+    if (!selectedJob) {
+      return null
+    }
+
+    return (
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={() => {
+          setIsModalVisible(false)
         }}
       >
-        <Text style={styles.addButtonText}>
-          {editingTask ? 'Edit Task' : 'Add Task'}
-        </Text>
-      </TouchableOpacity>
-      {/* Render the TaskModal component */}
-      <TaskModal
-        modalVisible={modalVisible}
-        task={task}
-        setTask={setTask}
-        handleAddTask={handleAddTask}
-        handleCancel={() => {
-          setEditingTask(null)
-          setTask({
-            title: '',
-            description: '',
-            status: 'Pending',
-            deadline: '',
-            createdAt: '',
-          })
-          setModalVisible(false)
-          setValidationError(false)
-        }}
-        validationError={validationError}
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>{selectedJob.title}</Text>
+            <Text style={styles.modalCompany}>{selectedJob.company}</Text>
+            <Text style={styles.modalLocation}>{selectedJob.location}</Text>
+            <Text style={styles.modalDescription}>
+              {selectedJob.description}
+            </Text>
+            <Text style={styles.modalApplyMethod}>
+              {selectedJob.applyMethod}
+            </Text>
+            <Button title="Close" onPress={() => setIsModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Job Board App</Text>
+      <Text style={styles.subHeading}>By Mayank Sharma</Text>
+
+      <View style={styles.filterContainer}>
+        {showFilters ? (
+          <>
+            <View style={styles.row}>
+              <View style={styles.filterColumn}>
+                <Text style={styles.filterLabel}>Title</Text>
+                <TextInput
+                  style={styles.filterInput}
+                  value={titleFilter}
+                  onChangeText={(text) => setTitleFilter(text)}
+                />
+              </View>
+
+              <View style={styles.filterColumn}>
+                <Text style={styles.filterLabel}>Company</Text>
+                <TextInput
+                  style={styles.filterInput}
+                  value={companyFilter}
+                  onChangeText={(text) => setCompanyFilter(text)}
+                />
+              </View>
+
+              <View style={styles.filterColumn}>
+                <Text style={styles.filterLabel}>Location</Text>
+                <TextInput
+                  style={styles.filterInput}
+                  value={locationFilter}
+                  onChangeText={(text) => setLocationFilter(text)}
+                />
+              </View>
+            </View>
+
+            <Button title="Apply Filters" onPress={applyFilters} />
+          </>
+        ) : (
+          <Button title="Show Filters" onPress={toggleFilters} />
+        )}
+      </View>
+
+      {/* <ScrollView> */}
+
+      <FlatList
+        data={jobData}
+        keyExtractor={(item) => item.id}
+        renderItem={renderJobItem}
+        ListHeaderComponent={() => (
+          <View style={{ padding: 16 }}>
+            <Text style={{ fontSize: 24 }}>Welcome to the Job Board!</Text>
+          </View>
+        )}
+        ListFooterComponent={() => (
+          <View style={{ padding: 16 }}>
+            <Text>That's all, folks!</Text>
+          </View>
+        )}
       />
+
+      {/* </ScrollView> */}
+      {renderJobDetailsModal()}
     </View>
   )
 }
 
-// Export the App component as the default export
-// export default App;component as the default export
-export default App
+const styles = StyleSheet.create({
+  container: {
+    padding: 26,
+    backgroundColor: '#fff',
+  },
+  header: {
+    marginVertical: 24,
+    fontSize: 34,
+    fontWeight: 'bold',
+    marginBottom: 26,
+  },
+  subHeading: {
+    color: 'green',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 90,
+    marginTop: -20,
+  },
+  filterContainer: {
+    marginBottom: 16,
+    marginTop: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  filterColumn: {
+    flex: 1,
+    marginRight: 8,
+    marginBottom: 16,
+  },
+  filterLabel: {
+    fontSize: 16,
+    marginBottom: 4,
+  },
+  filterInput: {
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+  },
+  jobItem: {
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  jobTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  jobCompany: {
+    fontSize: 16,
+    color: '#555',
+    marginBottom: 4,
+  },
+  jobLocation: {
+    fontSize: 16,
+    color: '#555',
+    marginBottom: 4,
+  },
+  jobDescription: {
+    fontSize: 14,
+    color: '#777',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 8,
+    width: '80%',
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  modalCompany: {
+    fontSize: 18,
+    color: '#555',
+    marginBottom: 8,
+  },
+  modalLocation: {
+    fontSize: 16,
+    color: '#555',
+    marginBottom: 8,
+  },
+  modalDescription: {
+    fontSize: 14,
+    color: '#777',
+    marginBottom: 8,
+  },
+  modalApplyMethod: {
+    fontSize: 14,
+    color: 'blue',
+    marginBottom: 8,
+  },
+})
+
+export default JobBoard
