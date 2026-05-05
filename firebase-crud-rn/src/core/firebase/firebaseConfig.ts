1e12ac6dac53ce7f81e873a07e065c51a82e4307
Mayank Sharma <mayankece155@gmail.com>
2026-05-05 19:22:47 +0530

    Small Typo Error Fix

--- firebase-crud-rn/src/core/firebase/firebaseConfig.ts
+++ firebase-crud-rn/src/core/firebase/firebaseConfig.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
 const app =
   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-let auth;
+let auth : any;
 
 try {
   const { getReactNativePersistence } = require("firebase/auth");
